84fa918fad5d62f942586aa0aa1c7a88e2c7950b
fdgongora@iesmurgi.org <fga432@inlumine.ual.es>
2021-10-22 12:43:29 +0200

    Creación de la estructura de carpetas y el Modelo

---
+++ src/main/java/com/example/javafxmvcsinbind/controllers/ActualizarPedido.java
@@ -0,0 +1,4 @@
+package com.example.javafxmvcsinbind.controllers;
+
+public class ActualizarPedido {
+}
---
+++ src/main/java/com/example/javafxmvcsinbind/models/Pedido.java
@@ -0,0 +1,13 @@
+package com.example.javafxmvcsinbind.models;
+
+import java.util.Date;
+
+public class Pedido {
+    private Integer id;
+    private Integer idCliente;
+    private Integer idTransportista;
+    private Date fecPedido;
+    private String observaciones;
+    private Boolean facturado;
+
+}
